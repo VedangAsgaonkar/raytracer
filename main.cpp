a6b09a88965adba259403108b3e59d72a412a9c8
Vedang Asgaonkar <vedanga2015@gmail.com>
2021-09-06 17:38:12 +0530

    a few nice images

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 int main(){
 
     //camera
-    vec3 lookfrom(-3,3,0);
+    vec3 lookfrom(-2,1,0.5);
     vec3 lookat(0,0,-1);
     vec3 roll(0,1,0);
     float vertical_theta = PI/4;
@@ -25,14 +25,15 @@ int main(){
     camera c(lookfrom,lookat,roll,vertical_theta, horizontal_theta, vertical_pixel, horizontal_pixel);
 
     //world
-    hitable* list[5];
+    hitable* list[6];
     list[0] = new sphere(vec3(0,0,-1), 0.5, new dielectric(1.5) );
-    list[1] = new sphere(vec3(0,-100.5,-1),100, new lambertian(vec3(0.8,0.8,0.0)));
-    list[2] = new sphere(vec3(1,0,-1),0.5,new metal(vec3(0.8,0.6,0.2),0.1));
-    list[3] = new sphere(vec3(-1,0,-1),0.5, new lambertian(vec3(0.8,0.3,0.3)) );
+    list[1] = new sphere(vec3(0,-100.5,-1),100, new metal(vec3(0.7,0.8,0.9),0.05));
+    list[2] = new sphere(vec3(1,0,-2),0.5,new metal(vec3(0.8,0.6,0.2),0.1));
+    list[3] = new sphere(vec3(-1,0,-1),0.5, new metal(vec3(0.8,0.6,0.2),0.1) );
     list[4] = new sphere(vec3(1,0,-8),0.45, new lambertian(vec3(0.3,0.3,0.8)));
-    hitable *world = new hitable_list(list,5);
+    list[5] = new sphere(vec3(0.5,0,0.5), 0.25, new lambertian(vec3(0.8,0.3,0.3)));
+    hitable *world = new hitable_list(list,6);
 
     //render
-    c.render(world,"images/secondImage.ppm");
+    c.render(world,"images/fifthImage.ppm");
 }
